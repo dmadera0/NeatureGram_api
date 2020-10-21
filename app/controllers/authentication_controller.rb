214ddee50c7f00b4f60b85644d4af0cb1e6b4d9e
Daniel <dmadera0@gmail.com>
2020-10-21 13:56:05 -0600

    add auth token

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationController < ApplicationController
     
     def login
-        user =User.find_by(name: params:[:name])
+        @user =User.find_by(name: params[:name])
 
        if !@user
         render json:{ message: 'That user does not exist'}, status: :unauthorized
@@ -9,7 +9,10 @@ class AuthenticationController < ApplicationController
         if !@user.authenticate params[:password]
             render json:{message: 'Bad password'}, status: :unauthorized
         else
-            render json: {user: @user}, status: :ok
+            payload = { user_id: @user.id}
+            secret = 'secret'
+            token = JWT.encode(payload, secret)
+            render json: { token: token}, status: :ok
        end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
 
-  post '/login', to: 'auth#login'
+  post '/login', to: 'authentication#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
